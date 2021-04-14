1001b73471cd76ad6b58b3d023683ffaba8287ee
JosephAntonioo <56083204+JosephAntonioo@users.noreply.github.com>
2021-04-13 22:13:39 -0300

    Consumo bd crawler

---
+++ backend/src/main/java/com/schema/backend/controller/CrawlerDataController.java
@@ -0,0 +1,27 @@
+package com.schema.backend.controller;
+
+import java.util.List;
+
+import com.schema.backend.entity.Crawler;
+import com.schema.backend.repository.CrawlerRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/crawler")
+@CrossOrigin
+public class CrawlerDataController {
+
+	@Autowired
+	private CrawlerRepository crawlerRepository;
+
+	@GetMapping("/listAll")
+	public List<Crawler> listAll(){
+		List<Crawler> list =  crawlerRepository.findAll();
+
+	}
+}
