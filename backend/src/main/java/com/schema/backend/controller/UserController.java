d99205126f814ae9febaf739aa2fb20e58ac00cf
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-04-14 17:46:29 -0300

    update. localhost OK

--- backend/src/main/java/com/schema/backend/controller/UserController.java
+++ backend/src/main/java/com/schema/backend/controller/UserController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import com.schema.backend.entity.User;
 import com.schema.backend.repository.UserRepository;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
